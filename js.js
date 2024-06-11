76070109388397290519c285ea14a791100031ae
Savely14 <141026829+Savely14@users.noreply.github.com>
2024-06-12 01:32:56 +0900

    12.06.24

--- js.js
+++ js.js
@@ -1,1 +1,34 @@
 ﻿
+const addTaskButton = document.querySelector('#addTaskButton') 
+const tasksButton = document.querySelector('#addTaskButton1') 
+const taskInput = document.querySelector('#taskInput') 
+const taskSection = document.querySelector('.taskSection') 
+const tasks = document.querySelector('.tasks') 
+const tasksTextField = document.querySelector('.tasksTextField') 
+const tasksLength = tasks.getElementsByTagName("*").length
+let inputData = {}
+const LS = localStorage
+
+
+
+
+addTaskButton.addEventListener('click', ()=> {
+    //taskInput1.value = addTaskInput.value
+    taskInput1.value = addTaskInput.value
+    console.log(taskInput);
+    addTaskInput.value = ''
+    // for (i=0; i < tasksLength; i++) {
+    //     tasks.children.value = "!"
+    //     console.log(tasks.children);
+
+    // }
+})
+
+// tasksButton.addEventListener('click', ()=> {
+//     taskInput1.value = ''
+// })
+
+
+
+    
+
